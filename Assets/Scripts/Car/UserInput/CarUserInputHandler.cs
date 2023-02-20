f1bc4fda0f61cbc68322bc0f89554f852c3e8aa2
Yuriy Danyliuk <56362999+yuradanyliuk@users.noreply.github.com>
2023-02-20 11:57:29 +0200

    Create user controlled car

---
+++ Assets/Scripts/Car/UserInput/CarUserInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace AutomaticParking.Car.UserInput
+{
+    public class CarUserInputHandler : MonoBehaviour
+    {
+        private const string VerticalAxisName = "Vertical";
+        private const string HorizontalAxisName = "Horizontal";
+        private const KeyCode BrakingKeyCode = KeyCode.Space;
+
+        [SerializeField] private CarData carData;
+        private CarUserInputInterpreter interpreter;
+
+        private void Awake() => interpreter = new CarUserInputInterpreter(carData);
+
+        private void Update()
+        {
+            carData.CurrentWheelTorque = interpreter.InterpretAsWheelTorque(Input.GetAxis(VerticalAxisName));
+            carData.CurrentSteeringAngle = interpreter.InterpretAsSteeringAngle(Input.GetAxis(HorizontalAxisName));
+            carData.IsBreaking = interpreter.InterpretAsBreakingState(Input.GetKey(BrakingKeyCode));
+        }
+    }
+}
---
+++ Assets/Scripts/Car/UserInput/CarUserInputInterpreter.cs
@@ -0,0 +1,15 @@
+﻿namespace AutomaticParking.Car.UserInput
+{
+    public class CarUserInputInterpreter
+    {
+        private readonly CarData carData;
+
+        public CarUserInputInterpreter(CarData carData) => this.carData = carData;
+
+        public float InterpretAsWheelTorque(float input) => input * carData.MaxWheelTorque;
+
+        public float InterpretAsSteeringAngle(float input) => input * carData.MaxSteeringAngle;
+
+        public bool InterpretAsBreakingState(bool input) => input;
+    }
+}
