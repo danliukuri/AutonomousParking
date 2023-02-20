b84102992c51cd894ad82a1bfb3d5c83ce5800af
Yuriy Danyliuk <56362999+yuradanyliuk@users.noreply.github.com>
2023-02-20 23:56:39 +0200

    Create car engine with needed data

---
+++ Assets/Scripts/Car/CarData.cs
@@ -0,0 +1,26 @@
+﻿using AutomaticParking.Car.Wheel;
+using UnityEngine;
+
+namespace AutomaticParking.Car
+{
+    public class CarData : MonoBehaviour
+    {
+        [field: SerializeField] public float MaxEngineTorque { get; private set; } = 100f;
+        [field: SerializeField] public float MaxBrakeTorque { get; private set; } = 1000f;
+        [field: SerializeField, Range(0, 90)] public float MaxSteeringAngle { get; private set; } = 35f;
+        [field: SerializeField, Range(0, 1)] public float DrivetrainEfficiency { get; private set; } = 0.75f;
+
+        public float MaxWheelTorque { get; private set; }
+        public float CurrentWheelTorque { get; set; }
+        public float CurrentSteeringAngle { get; set; }
+        public bool IsBreaking { get; set; }
+
+        private void Awake()
+        {
+            var wheelsInfo = GetComponentInChildren<WheelsData>();
+            int drivingWheelsCount = wheelsInfo.DrivingWheels.Length;
+            
+            MaxWheelTorque = MaxEngineTorque * DrivetrainEfficiency / drivingWheelsCount;
+        }
+    }
+}
---
+++ Assets/Scripts/Car/CarEngine.cs
@@ -0,0 +1,26 @@
+﻿using AutomaticParking.Car.Wheel;
+using UnityEngine;
+
+namespace AutomaticParking.Car
+{
+    [RequireComponent(typeof(CarData)), RequireComponent(typeof(WheelsOperator))]
+    public class CarEngine : MonoBehaviour
+    {
+        private CarData carData;
+        private WheelsOperator wheelsOperator;
+
+        private void Awake()
+        {
+            carData = GetComponent<CarData>();
+            wheelsOperator = GetComponent<WheelsOperator>();
+        }
+
+        private void FixedUpdate()
+        {
+            wheelsOperator.ApplyWheelTorque(carData.CurrentWheelTorque / Time.fixedDeltaTime);
+            wheelsOperator.ApplyBreaking(carData.IsBreaking ? carData.MaxBrakeTorque : default);
+            wheelsOperator.ApplySteering(carData.CurrentSteeringAngle);
+            wheelsOperator.SynchronizeWheelsVisualization();
+        }
+    }
+}
