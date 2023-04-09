dcf1d7917a82be4d464b677cafe0f5a0988bb1da
Yuriy Danyliuk <56362999+yuradanyliuk@users.noreply.github.com>
2023-04-09 20:07:29 +0300

    Fix undesired behaviour in Normalize extension

--- Assets/Scripts/Common/Extensions/FloatExtensions.cs
+++ Assets/Scripts/Common/Extensions/FloatExtensions.cs
@@ -1,8 +1,17 @@
-﻿namespace AutomaticParking.Common.Extensions
+﻿using UnityEngine;
+
+namespace AutomaticParking.Common.Extensions
 {
     public static class FloatExtensions
     {
-        public static float Normalize(this float value, float min, float max) => (value - min) / (max - min);
+        public static float Normalize(this float value, float min, float max)
+        {
+            const float minNormalizedValue = 0f, maxNormalizedValue = 1f;
+            float range = max - min;
+            float normalizedValue = Mathf.Approximately(range, default) ? minNormalizedValue : (value - min) / range;
+            normalizedValue = Mathf.Clamp(normalizedValue, minNormalizedValue, maxNormalizedValue);
+            return normalizedValue;
+        }
 
         public static float ChangeBounds(this float value, float oldMin, float oldMax, float newMin, float newMax) =>
             value.Normalize(oldMin, oldMax) * (newMax - newMin) + newMin;
