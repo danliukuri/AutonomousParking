53299fba34c99f139be1ca829d9a971a474e2840
Yuriy Danyliuk <56362999+yuradanyliuk@users.noreply.github.com>
2023-04-14 17:05:43 +0200

    Create agent target parking zone drawer

---
+++ Assets/Scripts/Editor/Agent/Data/ParkingAgentTargetParkingZoneDrawer.cs
@@ -0,0 +1,24 @@
+﻿using AutomaticParking.Agents.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace AutomaticParking.Editor.Agent.Data
+{
+    [CanEditMultipleObjects, CustomEditor(typeof(ParkingAgentTargetData))]
+    public class ParkingAgentTargetParkingZoneDrawer : UnityEditor.Editor
+    {
+        private readonly Color parkingZoneColor = Color.green;
+        private readonly Color perfectParkingZoneColor = Color.white;
+        
+        private void OnSceneGUI()
+        {
+            var data = (ParkingAgentTargetData)target;
+            Vector3 position = data.transform.position;
+            
+            Handles.color = parkingZoneColor;
+            Handles.DrawWireDisc(position, Vector3.up, data.ParkingRadius);
+            Handles.color = perfectParkingZoneColor;
+            Handles.DrawWireDisc(position, Vector3.up, data.PerfectParkingRadius);
+        }
+    }
+}
